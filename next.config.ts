2d647c66d096fa7e52bc96f3b8eed4ee01df296f
Devendra Singh Rao <devthedeveloperguy@gmail.com>
2025-01-18 12:50:39 +0530

    fix: async error in next config

--- next.config.ts
+++ next.config.ts
@@ -29,7 +29,9 @@ const nextConfig: NextConfig = {
 };
 
 if (env.NODE_ENV === "development") {
-    await setupDevPlatform();
+    (async () => {
+        await setupDevPlatform();
+    })();
 }
 
 export default nextConfig;
